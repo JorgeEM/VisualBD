bbdf8e40b91dd45220bb6697ed287b17091fa7d1
Kevin Jaramillo Lievano <k.e.jl@hotmail.com>
2018-04-13 19:17:56 -0500

    Proyecto BD + C#

---
+++ BD1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BD1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conectar = new MySqlConnection("server=127.0.0.1; database=PracticaVisual; Uid=root; pwd=;");            
+            try
+            {
+                comboBox1.Text = "Nombres";
+                comboBox1.Items.Clear();
+                conectar.Open();
+
+                MySqlCommand comando = new MySqlCommand("Select nombre from alumno",conectar);
+                MySqlDataReader almacena = comando.ExecuteReader();
+
+                while (almacena.Read())
+                {
+                    comboBox1.Refresh();
+                    comboBox1.Items.Add(almacena.GetValue(0).ToString());
+                }
+                conectar.Close();
+            }
+            catch(MySqlException r)
+            {
+                MessageBox.Show(r.Message);
+            }
+        }
+    }
+}
